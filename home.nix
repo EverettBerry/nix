6916d0701478f585581458a81b57431aba7e083e
jacobi petrucciani <j@cobi.dev>
2022-01-26 11:45:03 -0500

    trying to load from mods

--- home.nix
+++ home.nix
@@ -182,7 +182,7 @@ with pkgs.hax; {
         nix_hash_hm
 
         # sounds
-        cobi.hax.meme_sounds
+        cobi.meme_sounds
 
         (writeShellScriptBin "hms" ''
           ${pkgs.git}/bin/git -C ~/.config/nixpkgs/ pull origin main
